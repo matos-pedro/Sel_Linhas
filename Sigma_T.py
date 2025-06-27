7450650afa609f6a6b5f368b3f31279eb41e9f69
Pedro Matos <pedroasm1988@gmail.com>
2025-06-27 10:37:33 -0300

    Update Sigma_T.py

--- Sigma_T.py
+++ Sigma_T.py
@@ -176,7 +176,7 @@ with aba_explicacao:
 
     ##### 2.1 Calculando a Intensidade de Absorção
 
-    Em espectroscopia, a intensidade espectral observada $I(\\nu, T)$ resulta da atenuação da radiação ao longo de um meio absorvedor, conforme a Lei de Beer-Lambert:
+    A intensidade espectral observada $I(\\nu; T)$ resulta da atenuação da radiação ao longo de um meio absorvedor, conforme a Lei de Beer-Lambert:
 
     $$ 
     I(\\nu;T) = I_0(\\nu) \\cdot \\exp\\left[- \\alpha(\\nu;T) \\cdot L\\right]
