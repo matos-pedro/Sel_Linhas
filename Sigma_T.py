32e43b6c9fa4ccdd2fd8721bbac4ff1d28fc26ec
Pedro Matos <pedroasm1988@gmail.com>
2025-06-27 10:13:33 -0300

    Update Sigma_T.py

--- Sigma_T.py
+++ Sigma_T.py
@@ -178,9 +178,7 @@ with aba_explicacao:
 
         Em espectroscopia, a **intensidade espectral observada** $I(\nu, T)$ resulta da atenuação da radiação ao longo de um meio absorvedor, conforme a **Lei de Beer-Lambert**:
 
-        $$
-        I(\nu) = I_0(\nu) \cdot \exp\left[-\alpha(\nu) \cdot L\right]
-        $$
+       
 
 
     ---
